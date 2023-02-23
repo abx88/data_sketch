84a1d9c7669fce86d5ef0fd6c55f22c451369fa0
abx88 <a.bucalossi88@gmail.com>
2023-02-24 00:19:45 +0100

    Update data_wrap.py

--- data_wrap.py
+++ data_wrap.py
@@ -11,27 +11,24 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     delimitatore = st.text_input("delimitatore", "s")
     st.write("delimiter is ", delimitatore)
-    # Can be used wherever a "file-like" object is accepted:
-            
-    df = pd.read_csv(uploaded_file, delimiter = (delimitatore))
-    
-    # Rinomina le colonne con numeri in ordine crescente
-    new_column_names = list(range(len(df.columns)))
-    df.columns = new_column_names
+    if delimitatore is not None:
+        df = pd.read_csv(uploaded_file, delimiter = (delimitatore))
+        # Rinomina le colonne con numeri in ordine crescente
+        new_column_names = list(range(len(df.columns)))
+        df.columns = new_column_names
+        st.write(df)
 
-    st.write(df)
+        @st.cache
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
 
-    @st.cache
-    def convert_df(df):
-        # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode('utf-8')
+        csv = convert_df(df)
 
-    csv = convert_df(df)
-
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name='large_df.csv',
-        mime='text/csv')
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='large_df.csv',
+            mime='text/csv')
 else:
     st.text("inserire file csv")
