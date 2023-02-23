4861c05104c578d6f510e3488abb9bca644f06f9
abx88 <a.bucalossi88@gmail.com>
2023-02-24 00:29:32 +0100

    Update data_wrap.py

--- data_wrap.py
+++ data_wrap.py
@@ -32,7 +32,7 @@ if uploaded_file is not None:
             file_name='large_df.csv',
             mime='text/csv')
     else:
-        df = pd.read_csv(uploaded_file, delimiter = (delimitatore))
+        df = pd.read_csv(uploaded_file)
         st.text("delimitatore non valido")
 else:
     st.text("inserire file csv")
