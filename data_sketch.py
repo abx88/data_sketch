7a4c9d8827aac0b1ec6e07d07cc6b3d33090eb6e
abx88 <a.bucalossi88@gmail.com>
2023-02-28 23:43:48 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -81,7 +81,7 @@ if uploaded_file is not None:
     col2.subheader("visualizza variabile")
     colonna_da_visualizzare = col2.selectbox("Seleziona le colonne da visualizzare", newdf.columns.tolist())
     #col2.line_chart(int(colonna_da_visualizzare))
-    col2.line_chart(newdf.colonna_da_visualizzare)
+    col2.line_chart(colonna_da_visualizzare)
 
     
     
