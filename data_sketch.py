122cd5b7e60da1fd3ae6003ed9744ec3fb62642f
abx88 <125708116+abx88@users.noreply.github.com>
2023-03-03 09:49:48 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -98,7 +98,7 @@ if uploaded_file is not None:
             # elimina le righe che contengono i valori selezionati
             newdf = newdf.loc[~newdf[scegli_colonna_valori].isin(valori_da_elim)]
 
-    newdf = st.experimental_data_editor(newdf, num_rows=”dynamic”)        
+    newdf = st.experimental_data_editor(newdf, num_rows="dynamic")        
     st.experimental_data_editor(newdf, key="data_editor") #  Set a key
     col2.write(newdf)       
     #col2.write(newdf)
