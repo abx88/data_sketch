ea2eb47dcb923b6b4ef6f352c8ea78eee9773e9e
abx88 <a.bucalossi88@gmail.com>
2023-03-05 19:20:38 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -272,13 +272,14 @@ if uploaded_file is not None:
                         x=x, 
                         y=pdf, 
                         mode='lines', 
-                        name='distribuzione normale'))
+                        name='distribuzione normale',
+                        yaxis='y2))
 
                     # aggiungi i titoli degli assi e il titolo della figura
                     distribuzione_perc.update_layout(
                         xaxis_title_text=colonna_distribuzione,
                         yaxis_title_text='densità di probabilità',
-                        yaxis2=dict(overlaying='x',side='right'),
+                        yaxis2=dict(overlaying='y',side='right'),
                         title={
                             'text': "Distribuzione dei dati e distribuzione normale",
                             'y':0.9,
