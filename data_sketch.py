e19b308d494ee0f39ab18c87f38fe28bdd93a003
abx88 <a.bucalossi88@gmail.com>
2023-03-08 00:06:36 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -241,8 +241,7 @@ if uploaded_file is not None:
                     graficoBarre.add_trace(go.Bar(
                         y = pivotVariabile[colonna_confrontoY],
                         x = pivotVariabile[colonna_confrontoY],
-                        name="grafico a barre",
-                        connectgaps=False))
+                        name="grafico a barre")
 
                     graficoBarre.update_layout(
                         xaxis_title_text=colonna_confrontoX,
