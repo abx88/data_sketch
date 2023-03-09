9b56d5b7d0a15d3a107cd2e9a20aea6907ccb9cc
abx88 <a.bucalossi88@gmail.com>
2023-03-10 00:02:55 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -15,13 +15,8 @@ st.set_page_config(
     page_title="DataSketch",
     layout="wide")
 
-#dfprova = pd.DataFrame(np.random.randn(50, 5), columns=('col %d' % i for i in range(5)))
-a = (1,2,4,6,7)
-b = ("f","f","f","h","h")
-c = (3,4,4,6,8)
-indice = list(range(3))
-
-dfprova = pd.DataFrame((a,b,c), index = indice)
+np.random.seed(123)
+dfprova = pd.DataFrame(np.random.randn(50, 5), columns=('col %d' % i for i in range(5)))
 
 st.header("Data Sketch")
 
