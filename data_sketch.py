07fa10a95c19dc8c8c4a1f302713c409bef29fd2
abx88 <a.bucalossi88@gmail.com>
2023-03-09 23:13:14 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -21,10 +21,11 @@ st.header("Data Sketch")
 pagina = st.radio("selezionare operazione",('modifica ed esporta','visualizzazione'))
 col1, col2 = st.columns([2, 2])
 expander_modificheCol = col1.expander("filtra/modifica colonne")
-expander_modificheRighe = col2.expander("filta/modifica righe")
+expander_modificheRighe = col2.expander("filtra/modifica righe")
+expander_csv = st.expander("dati")
 expander_modificheCol.write("modifiche effettuate su colonne")    
 expander_modificheRighe.write("modifiche effettuate su righe")
-
+expander_csv.write("dati")
 uploaded_file = st.file_uploader("Selezionare un file .csv/.txt")
 if uploaded_file is not None:
     st.sidebar.header("Tool Modifica")
@@ -35,10 +36,10 @@ if uploaded_file is not None:
     # Aggiungi l'elemento checkbox per selezionare il delimitatore
     delimitatore= st.sidebar.radio("Seleziona il delimitatore", delimiter_options)
     # Aggiungi nomi colonne dataset
-    col1.subheader("dataset originale")
+    expander_csv.col1.subheader("dataset originale")
     df = pd.read_csv(uploaded_file, delimiter = delimitatore)
-    dfedit = col1.experimental_data_editor(df, num_rows="dynamic")
-    col2.subheader("dataset modificato")
+    dfedit = expander_csv.col1.experimental_data_editor(df, num_rows="dynamic")
+    expander_csv.col2.subheader("dataset modificato")
     newdf= dfedit
    
     
@@ -129,7 +130,7 @@ if uploaded_file is not None:
             newdf = newdf.loc[newdf[scegli_colonna_valori_filtro].isin(valori_da_filtrare)]
     
     
-    col2.write(newdf)       
+    expander_csv.col2..write(newdf)       
     newdfvisual=newdf
 
     
