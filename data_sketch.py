940a607bfb8a50bec6188007526b7745d05fa3be
abx88 <a.bucalossi88@gmail.com>
2023-03-19 16:53:36 +0100

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -141,18 +141,12 @@ if righe_da_filtrare:
 #aggiunta colonne
 expander_colonne.write("aggiunta guidata colonne")
 
-def create_max_column(df):
-    expander_colonne.write('Seleziona la colonna:')
+if expander_colonne.button('Crea nuova colonna', on_click=False):
     column = expander_colonne.selectbox('', options=df.columns)
     new_col_name = expander_colonne.text_input('Inserisci il nome della nuova colonna:')
     max_values = df[column].max()
     df[new_col_name] = max_values
-    expander_colonne.write('Colonna creata con successo!')
-    return df
     
-if expander_colonne.button('Crea nuova colonna', on_click=False):
-    newdf = create_max_column(newdf)
-   
         
 pivot_df = st.sidebar.checkbox("raggruppa dati")
 if pivot_df == True:
