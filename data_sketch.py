9b60329307e2282993b721eac107be3b66e95652
abx88 <125708116+abx88@users.noreply.github.com>
2023-07-22 18:19:37 +0200

    Update data_sketch.py

--- data_sketch.py
+++ data_sketch.py
@@ -126,8 +126,8 @@ if converti == True:
     colonne_da_convertire = expander_convertire.selectbox("Seleziona le colonne da convertire", newdf.columns.tolist())
     # converte i valori delle colonne selezionate in float
     for colonna in colonne_da_convertire:
-        newdf[colonna] = newdf[colonna].replace(".", "")
-        newdf[colonna] = newdf[colonna].replace(",", ".")
+        newdf[colonna] = newdf[colonna].replace('.', '')
+        newdf[colonna] = newdf[colonna].replace(',', '.')
         newdf[colonna] = newdf[colonna].astype(float)
 
 righe_da_eliminare = expander_modificheRighe.checkbox("righe da eliminare")
